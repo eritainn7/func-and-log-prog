6c9c083cfb78ecd28c9584fe1e147b179bd140d1
eritainn7 <eritainn7@gmail.com>
2026-03-08 21:20:33 +0300

    feat: complete task 3

--- f_sharp/LR5/Program.fs
+++ f_sharp/LR5/Program.fs
@@ -10,3 +10,20 @@ let SolveQuadEq a b c =
     [(-b + sqrt D)/(2.0 * a); (-b - sqrt D)/(2.0 * a)]
 
 //Задание 3
+open System
+
+[<EntryPoint>]
+let main argv=
+    let RoundSquare r = Math.PI * r * r
+    
+    let VolumeCylinder r h =
+        (RoundSquare r) * h
+
+    printfn "%f" (VolumeCylinder 5.0 12.45)
+
+    //Каррирование
+    let VolumeCylinderFixedR = VolumeCylinder 5.0
+    let volume = VolumeCylinderFixedR 12.45
+    printfn "%f" volume
+
+    0
