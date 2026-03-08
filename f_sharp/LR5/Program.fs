a35f6bd6d253dddbbb33b2737e04f92e24e03c86
eritainn7 <eritainn7@gmail.com>
2026-03-08 19:54:23 +0300

    fix: delete call of printHelloWorld

--- f_sharp/LR5/Program.fs
+++ f_sharp/LR5/Program.fs
@@ -1,10 +1,12 @@
 ﻿// Задание1
 let printHelloWorld = printfn "Hello, World!"
 
-printHelloWorld
+
 
 //Задание 2
 let SolveQuadEq a b c =
     let D = b * b - 4.0 * a * c
     
     [(-b + sqrt D)/(2.0 * a); (-b - sqrt D)/(2.0 * a)]
+
+//Задание 3
